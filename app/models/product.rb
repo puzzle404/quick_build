27d8fb98346e10849a9e004640731c09a275c79b
Carlos Manuel Ferrer <94634890+puzzle404@users.noreply.github.com>
2025-08-05 22:24:44 -0300

    Add categories and products management

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   acts_as_tenant(:company)
   belongs_to :company
+  belongs_to :category
   has_many_attached :images
   
   validates :name, :price_cents, presence: true
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   subject { build(:product) }
-  
+
   it { should belong_to(:company) }
+  it { should belong_to(:category) }
   it { should validate_presence_of(:name) }
 end
