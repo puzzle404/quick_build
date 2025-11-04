a0f1dc1279945be442ba1d82cad1b63ad039d21c
Carlos Manuel Ferrer <94634890+puzzle404@users.noreply.github.com>
2025-11-04 02:55:33 -0300

    fix in stimulus setup

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,18 @@
+
+
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+// eagerLoadControllersFrom("controllers", application)
+
+import HelloController from "controllers/hello_controller"
+import ProjectMapController from "controllers/project_map_controller"
+import SearchController from "controllers/search_controller"
+import SearchSubmitController from "controllers/search_submit_controller"
+import ToggleFormController from "controllers/toggle_form_controller"
+
+application.register("hello", HelloController)
+application.register("project-map", ProjectMapController)
+application.register("search", SearchController)
+application.register("search-submit", SearchSubmitController)
+application.register("toggle-form", ToggleFormController)
