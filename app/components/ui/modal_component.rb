83221f79ad7cf637feefbbcf904611960370640c
Manuel Ferrer <cmanuferrer@gmail.com>
2025-11-09 23:53:30 -0300

    modal for new forms

---
+++ app/components/ui/modal_component.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Ui::ModalComponent < ViewComponent::Base
+  PANEL_WIDTH = {
+    md: "max-w-3xl",
+    lg: "max-w-4xl",
+    xl: "max-w-5xl"
+  }.freeze
+
+  def initialize(title:, subtitle: nil, tag_label: nil, size: :xl, body_padding: "px-6 py-6 sm:px-10 sm:py-8", auxiliary_button: nil)
+    @title = title
+    @subtitle = subtitle
+    @tag_label = tag_label
+    @panel_width_class = PANEL_WIDTH.fetch(size, PANEL_WIDTH[:xl])
+    @body_padding = body_padding
+    @auxiliary_button = auxiliary_button&.symbolize_keys
+  end
+
+  attr_reader :body_padding, :title, :subtitle, :tag_label, :auxiliary_button
+
+  private
+
+  attr_reader :panel_width_class
+
+  def header?
+    title.present? || subtitle.present? || tag_label.present? || auxiliary_button.present?
+  end
+end
